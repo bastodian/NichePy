3021cc46e0e62586468e9feade99eab35afc8160
Bastian Bentlage <bastodian@gmail.com>
2011-11-29 13:48:00 -0600

    fixed some typos in the documentation

--- BatchScriptsAndWrappers/MaxentBatchExample.sh
+++ BatchScriptsAndWrappers/MaxentBatchExample.sh
@@ -9,7 +9,7 @@
 ### and autorun make Maxent run without interruptions such as errors due to some occurrence data that miss environmental information etc.
 ###
 ### Options for Maxent change frequently and the options in this example may not work in your version of Maxent. Start the Maxent GUI and click on the help button to learn more
-### more about teh flags supported by your version of Maxent.
+### more about the flags supported by your version of Maxent.
 
 java -jar /path/to/maxent.jar -e /path/to/environemntal/layers1/ projectionlayers=/path/to/projection/layers1/ samplesfile=/path/to/occurrences1.csv -o /path/to/output/directory1/ -z nowarnings notooltips autorun
 java -jar /path/to/maxent.jar -e /path/to/environemntal/layers2/ projectionlayers=/path/to/projection/layers2/ samplesfile=/path/to/occurrences2.csv -o /path/to/output/directory2/ -z nowarnings notooltips autorun
